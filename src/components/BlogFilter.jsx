7358b36727bf487063a26f21899868cfe7ceb3d5
Andreas Chrysopoulos <andreaschrysopoulos@gmail.com>
2025-02-27 12:38:58 +0200

    Enhance BlogFilter component with dark mode support and improve "No matching articles found" message styling

--- src/components/BlogFilter.jsx
+++ src/components/BlogFilter.jsx
@@ -10,21 +10,21 @@ const BlogFilter = ({ selected, setSelected }) => {
   };
 
   const isToggled = (category) => {
-    return selected.includes(category) ? 'bg-stone-200' : 'bg-stone-100 hover:bg-stone-200'
+    return selected.includes(category) ? 'bg-stone-200 dark:bg-stone-800' : 'bg-stone-100 dark:bg-stone-950 hover:bg-stone-200 dark:hover:bg-stone-800'
   }
 
   return (
     <div className='flex flex-wrap gap-2 overflow-x-auto items-center'>
-      <div className={`${isToggled('ECONOMICS')} border border-stone-200 px-4 py-1 rounded-full cursor-pointer transitions-all duration-200`} onClick={() => toggleCategory("ECONOMICS")}>Economics</div>
+      <div className={`${isToggled('ECONOMICS')} border border-stone-200 dark:border-stone-800 px-4 py-1 rounded-full cursor-pointer transitions-all duration-200`} onClick={() => toggleCategory("ECONOMICS")}>Economics</div>
 
-      <div className={`${isToggled('INTERNATIONAL')} border border-stone-200 px-4 py-1 rounded-full cursor-pointer transitions-all duration-200`} onClick={() => toggleCategory("INTERNATIONAL")}>International</div>
+      <div className={`${isToggled('INTERNATIONAL')} border border-stone-200 dark:border-stone-800 px-4 py-1 rounded-full cursor-pointer transitions-all duration-200`} onClick={() => toggleCategory("INTERNATIONAL")}>International</div>
 
-      <div className={`${isToggled('FINANCIAL')} border border-stone-200 px-4 py-1 rounded-full cursor-pointer transitions-all duration-200`} onClick={() => toggleCategory("FINANCIAL")}>Financial</div>
+      <div className={`${isToggled('FINANCIAL')} border border-stone-200 dark:border-stone-800 px-4 py-1 rounded-full cursor-pointer transitions-all duration-200`} onClick={() => toggleCategory("FINANCIAL")}>Financial</div>
 
-      <div className={`${isToggled('BUSINESS')} border border-stone-200 px-4 py-1 rounded-full cursor-pointer transitions-all duration-200`} onClick={() => toggleCategory("BUSINESS")}>Business</div>
+      <div className={`${isToggled('BUSINESS')} border border-stone-200 dark:border-stone-800 px-4 py-1 rounded-full cursor-pointer transitions-all duration-200`} onClick={() => toggleCategory("BUSINESS")}>Business</div>
 
       {
-        selected.length ? <div className='cursor-pointer px-3 opacity-50 hover:opacity-90 text-xl transition-opacity duration-200' onClick={() => setSelected([])}>X</div> : ""
+        selected.length ? <div className='cursor-pointer px-2 opacity-40 hover:opacity-80 dark:opacity-50 dark:hover:opacity-90 dark:text-white text-sm transition-opacity duration-200' onClick={() => setSelected([])}>Clear</div> : ""
       }
     </div>
 
