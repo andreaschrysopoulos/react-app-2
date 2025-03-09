992380c5ed11009deafb08648ea8ca79af3d52a4
Andreas Chrysopoulos <andreaschrysopoulos@gmail.com>
2025-03-09 13:55:31 +0200

    Refactor BlogFilter component for improved layout and styling consistency
    Added animations, improved padding and margins

--- src/components/BlogFilter.jsx
+++ src/components/BlogFilter.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useState } from 'react'
 
 const BlogFilter = ({ selected, setSelected }) => {
 
@@ -15,15 +14,15 @@ const BlogFilter = ({ selected, setSelected }) => {
 
   return (
     <div className='flex gap-2 overflow-x-auto items-center justify-between'>
-      <div className="flex gap-2 items-center dark:bg-stone-900 bg-stone-200 py-1.5 px-2 rounded-full">
+      <div className="flex items-center dark:bg-stone-900 bg-stone-200 py-1 px-1 rounded-full">
 
-        <div className={`${isToggled('economics')} border-stone-200 dark:border-stone-800 px-4 py-1 rounded-full cursor-pointer transitions-all duration-200`} onClick={() => toggleCategory("economics")}>Economics</div>
-        <div className={`${isToggled('international')} border-stone-200 dark:border-stone-800 px-4 py-1 rounded-full cursor-pointer transitions-all duration-200`} onClick={() => toggleCategory("international")}>International</div>
-        <div className={`${isToggled('financial')} border-stone-200 dark:border-stone-800 px-4 py-1 rounded-full cursor-pointer transitions-all duration-200`} onClick={() => toggleCategory("financial")}>Financial</div>
-        <div className={`${isToggled('business')} border-stone-200 dark:border-stone-800 px-4 py-1 rounded-full cursor-pointer transitions-all duration-200`} onClick={() => toggleCategory("business")}>Business</div>
-        {
-          selected.length ? <div className='cursor-pointer px-2 opacity-40 hover:opacity-80 dark:opacity-50 dark:hover:opacity-90 text-sm transition-opacity duration-200' onClick={() => setSelected([])}>Clear</div> : ""
-        }
+        <div className={`${isToggled('economics')} mr-1 border-stone-200 dark:border-stone-800 px-3 py-1 rounded-full cursor-pointer transition-all duration-200`} onClick={() => toggleCategory("economics")}>Economics</div>
+        <div className={`${isToggled('international')} mr-1 border-stone-200 dark:border-stone-800 px-3 py-1 rounded-full cursor-pointer transition-all duration-200`} onClick={() => toggleCategory("international")}>International</div>
+        <div className={`${isToggled('financial')} mr-1 border-stone-200 dark:border-stone-800 px-3 py-1 rounded-full cursor-pointer transition-all duration-200`} onClick={() => toggleCategory("financial")}>Financial</div>
+        <div className={`${isToggled('business')} border-stone-200 dark:border-stone-800 px-3 py-1 rounded-full cursor-pointer transition-all duration-200`} onClick={() => toggleCategory("business")}>Business</div>
+
+        <div className={`text-sm cursor-pointer transition-all duration-400 ease-in-out ${selected.length ? 'ml-0.5 max-w-20 w-fit px-2 opacity-40 dark:opacity-50 hover:opacity-80 dark:hover:opacity-90' : 'px-0 max-w-0 overflow-hidden opacity-0'}`} onClick={() => setSelected([])}>Clear</div>
+
       </div>
       <input id='search' className='transition-all duration-200 ease-in-out text-sm bg-stone-200 dark:bg-stone-900 rounded-full px-3.5 py-1.5  border-stone-800 focus:outline-0 focus:dark:bg-stone-800 focus:bg-stone-300' type="text" placeholder='Search' />
     </div>
