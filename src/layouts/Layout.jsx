591e4b2d4ae9231668d1290556dc163700979fea
Andreas Chrysopoulos <andreaschrysopoulos@gmail.com>
2025-03-14 19:51:47 +0200

    Remove Next.js dependency and integrate Vercel Analytics into main application structure

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Copyright from "../components/Copyright";
 import Widgets from "../components/Widgets";
-import { Analytics } from '@vercel/analytics/next';
 
 
 const Layout = () => {
@@ -12,7 +11,6 @@ const Layout = () => {
       <Outlet />
       <Widgets />
       <Copyright />
-      <Analytics />
     </>
   );
 };
