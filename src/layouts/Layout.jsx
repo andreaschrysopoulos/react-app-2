aa8898934cd2006ea6daedbbaba494a6135e2478
Andreas Chrysopoulos <andreaschrysopoulos@gmail.com>
2025-03-14 19:42:05 +0200

    Add Vercel Analytics dependency and integrate into Layout component

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -2,6 +2,8 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Copyright from "../components/Copyright";
 import Widgets from "../components/Widgets";
+import { Analytics } from '@vercel/analytics/next';
+
 
 const Layout = () => {
   return (
@@ -10,6 +12,7 @@ const Layout = () => {
       <Outlet />
       <Widgets />
       <Copyright />
+      <Analytics />
     </>
   );
 };
