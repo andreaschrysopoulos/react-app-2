bf3c8917ef355099c385ac660dc127fd352b42e7
Andreas Chrysopoulos <andreaschrysopoulos@gmail.com>
2025-03-19 00:30:33 +0200

    Update image file formats and remove unused images for consistency and optimization

--- src/assets/database.jsx
+++ src/assets/database.jsx
@@ -32,7 +32,7 @@ export const members = [
   {
     linkedin: "https://www.linkedin.com/in/ioannis-stafylidis/",
     key: 1,
-    img: "giannis.png",
+    img: "giannis.jpg",
     name: "Giannis Stafilidis",
     role: "President",
     bio: "As the President of the Board of Directors of our organization, I am a graduate of the Department of International and European Studies at the University of Macedonia and currently an undergraduate student at the Faculty of Law at Aristotle University of Thessaloniki. At the same time, I work as a Marketing Manager at Asklipios Medica, contributing to the development of promotional strategies for private enterprises. My journey within the organization has introduced me to the key values of responsibility and solidarity, while my previous tenure as a Marketing Manager highlighted the constant need to analyze external business environment stimuli and continuously improve the organization's public image. Ultimately, having participated in numerous volunteer organizations and institutions, my goal is the continuous improvement of INEFAN's work quality through ongoing initiatives and training programs for its members, enabling them to meet the modern challenges of the workplace and the business world.",
@@ -40,7 +40,7 @@ export const members = [
   {
     linkedin: "https://www.linkedin.com/in/elli-tsikrika/",
     key: 2,
-    img: "elli.png",
+    img: "elli.jpg",
     name: "Elli Tsikrika",
     role: "Head of Human Resources",
     bio: "As a professional in the field of Human Resources, I first came into contact with the organization about three years ago during my studies in the MSc in Human Resource Management at the University of Macedonia. Having already completed my studies in the Department of International and European Studies, I currently serve as the Team Leader of the HR department at INEFAN while also working as an HR Manager at one of the largest IT companies, Lancom. Additionally, I oversee the recruitment process for Battlenet, which is part of the same group. My passion for human resource management has led me to the position I hold today, while my volunteer work at INEFAN provided me with the opportunity to connect with my current employers. Finally, without a doubt, I feel it is my duty to share my knowledge with new volunteers and guide them in their development as professionals in the HR field.",
@@ -48,7 +48,7 @@ export const members = [
   {
     linkedin: "https://www.linkedin.com/in/apostolos-nikolaou-635775327/",
     key: 3,
-    img: "apostolos.png",
+    img: "apostolos.jpg",
     name: "Apostolos Nikolaou",
     role: "Head of Marketing",
     bio: "I am a graduate of the Department of International and European Studies at the University of Macedonia and the Team Leader of the Marketing team in our organization. As a team leader, I specialize in creating both print and digital promotional materials for INEFAN, while my goal is to continuously support my team members in acquiring knowledge and developing creative content. At the same time, I strive to enhance the organizational skills of the team members to help them understand the needs and dynamics of the marketing sector, as well as to communicate effectively with the broader audience.",
@@ -56,7 +56,7 @@ export const members = [
   {
     linkedin: "https://www.linkedin.com/in/niky-papanikolaou/",
     key: 4,
-    img: "niki.png",
+    img: "niki.jpg",
     name: "Niki Papanikolaou",
     role: "Head of P.M & Fundraising",
     bio: "I am a final-year student at the Faculty of Law at Aristotle University of Thessaloniki and a postgraduate MBA student specializing in Banking & Finance at ACT College. Having served as a Project & Fundraising Manager at TEDxAUTH and the Entrepreneurship Hub at ACT, my experience led me to the equally significant role of team leader in the Project Management & Fundraising team within our organization. In this capacity, my goal is to contribute to the organization's initiatives and share my knowledge and experience with the team members, enhancing collaboration and the effectiveness of our projects.",
@@ -64,7 +64,7 @@ export const members = [
   {
     linkedin: "https://www.linkedin.com/in/theodorostsoulfaidis/",
     key: 5,
-    img: "theodoros.png",
+    img: "theodoros.jpg",
     name: "Theodoros Tsoulfaidis",
     role: "Head of Financial Analysis",
     bio: "I am in the fourth year of my undergraduate studies in the Department of Economics at Aristotle University of Thessaloniki. With three years of active participation in the INEFAN organization, my academic education, combined with the experience I have gained, led me to take on the role of head of the economic analysis team within our organization. My main goal is to share my knowledge and experience with the team members while continuously developing and refining the skills required for analyzing indicators and variables in national and international markets.",
@@ -72,7 +72,7 @@ export const members = [
   {
     linkedin: "https://www.linkedin.com/in/thomas-sioumpalas-77a130231/",
     key: 6,
-    img: "thomas.png",
+    img: "thomas.jpg",
     name: "Thomas Sioumpalas",
     role: "Head of P.M & Fundraising",
     bio: "I am a student in the Department of Accounting and Information Systems at the International Hellenic University, while also working as an accounting assistant at LPC S.A., a member of the Motor Oil Group. My professional experience, combined with the knowledge I acquire from my academic studies, allows me to gain an in-depth understanding of the financial and managerial aspects of the modern business environment. Having been an active member of INEFAN for several years, I now have the honor of co-leading the Project Management & Fundraising team alongside Niki. My primary goal is the continuous expansion of our network of collaborators through strategic partnerships and targeted initiatives that strengthen the organization's impact. At the same time, I focus on the continuous development of team members, with particular emphasis on enhancing their communication and negotiation skills, enabling them to effectively navigate challenges and opportunities in the field of Project Management.",
@@ -91,7 +91,7 @@ export const articles = [
     category: "economics",
     title: "Seeking a New Productive Model",
     date: "November 29, 2024",
-    picture: "blog-2.png",
+    picture: "blog-2.jpg",
     key: "a2",
   },
   {
