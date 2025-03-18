1b4c8aa89b0cd90326f73f6850e0cc789d55c1c5
Andreas Chrysopoulos <andreaschrysopoulos@gmail.com>
2025-03-19 00:08:01 +0200

    Refactor PersonCard and Team components for improved layout and styling

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -5,18 +5,13 @@ const Team = () => {
   return (
     <>
       <div className="flex flex-auto pb-20 px-5">
-        <div className="flex flex-col mx-auto max-w-5xl">
-
-          {/* Page Header */}
-          {/* <span className="team3:text-5xl text-5xl font-semibold pb-3 mb-5 self-start w-min team2:w-full">
-            Board of Directors
-          </span> */}
+        <div className="flex flex-col mx-auto max-w-5xl w-min">
 
           <span className='font-semibold text-6xl mb-2'>Inefan Board</span>
           <span className='font text-3xl mb-10 dark:text-stone-400 text-stone-500'>The Board of Directors at Inefan leads with diverse expertise in economics, finance, and entrepreneurship, guiding our mission to connect youth with education and career opportunities.</span>
 
           {/* Members */}
-          <div className="grid gap-10 grid-cols-1 team2:grid-cols-2 team3:grid-cols-3 self-center">
+          <div className="grid gap-10 grid-cols-1 team2:grid-cols-2 team3:grid-cols-3 self-center min-w-70 team2:min-w-166 team3:min-w-255">
             {members.map((member) => (
               <CardTeamMember
                 key={member.key}
