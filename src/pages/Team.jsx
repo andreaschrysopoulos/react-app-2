d59f166bd26685de2b1db1adbb4689ece9c0ba93
Andreas Chrysopoulos <andreaschrysopoulos@gmail.com>
2025-03-19 00:08:47 +0200

    Update font sizes in Team component for improved readability and responsiveness

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -7,8 +7,8 @@ const Team = () => {
       <div className="flex flex-auto pb-20 px-5">
         <div className="flex flex-col mx-auto max-w-5xl w-min">
 
-          <span className='font-semibold text-6xl mb-2'>Inefan Board</span>
-          <span className='font text-3xl mb-10 dark:text-stone-400 text-stone-500'>The Board of Directors at Inefan leads with diverse expertise in economics, finance, and entrepreneurship, guiding our mission to connect youth with education and career opportunities.</span>
+          <span className='font-semibold team3:text-6xl text-5xl mb-2'>Inefan Board</span>
+          <span className='font team3:text-3xl text-2xl mb-10 dark:text-stone-400 text-stone-500'>The Board of Directors at Inefan leads with diverse expertise in economics, finance, and entrepreneurship, guiding our mission to connect youth with education and career opportunities.</span>
 
           {/* Members */}
           <div className="grid gap-10 grid-cols-1 team2:grid-cols-2 team3:grid-cols-3 self-center min-w-70 team2:min-w-166 team3:min-w-255">
